e023b9b25ec38978d868e959b2c282ed4a965667
Iryna Krush <iryna.krush@oa.edu.ua>
2026-04-07 00:00:49 +0300

    final fix terraform

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "vercel_project" "lab_deployment" {
+  name      = "lab6-terraform-final"
+  framework = "vite"
+  git_repository = {
+    type = "github"
+    repo = "Ira-name/lab-1-setup"
+  }
+}
+
+resource "vercel_project_domain" "custom_domain" {
+  project_id = vercel_project.lab_deployment.id
+  domain     = "lab6-final${var.student_id}.vercel.app"
+}
